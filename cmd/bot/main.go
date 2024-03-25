d4c92e1e33cbcf8e4dee4a64c25212901b601821
Primeraaizen <diyas.nurullaev@gmail.com>
2024-03-25 16:14:38 +0500

    Bot works

--- cmd/bot/main.go
+++ cmd/bot/main.go
@@ -1,26 +1,24 @@
 package main
 
-// import (
-//
-//	"log"
-//
-//	"github.com/workshop-1-bot/internal/clients/tg"
-//	"github.com/workshop-1-bot/internal/config"
-//	"github.com/workshop-1-bot/internal/model/messages"
-//
-// )
+import (
+	"log"
+	"workshop1-bot/internal/clients/tg"
+	"workshop1-bot/internal/config"
+	"workshop1-bot/internal/model/messages"
+)
+
 func main() {
-	//	config, err := config.New()
-	//	if err != nil {
-	//		log.Fatal("config init failed:", err)
-	//	}
-	//
-	//	tgClient, err := tg.New(config)
-	//	if err != nil {
-	//		log.Fatal("tg client init failed:", err)
-	//	}
-	//
-	//	msgModel := messages.New(tgClient)
-	//
-	//	tgClient.ListenUpdates(msgModel)
+	config, err := config.New()
+	if err != nil {
+		log.Fatal("config init failed:", err)
+	}
+
+	tgClient, err := tg.New(config)
+	if err != nil {
+		log.Fatal("tg client init failed:", err)
+	}
+
+	msgModel := messages.New(tgClient)
+
+	tgClient.ListenUpdates(msgModel)
 }
--- internal/clients/tg/tgclient.go
+++ internal/clients/tg/tgclient.go
@@ -1,17 +1,15 @@
 package tg
 
 import (
-	"github.com/pkg/errors"
 	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
+	"github.com/pkg/errors"
 	"log"
 	"workshop1-bot/internal/model/messages"
 )
 
-type TokenGetter struct {
+type TokenGetter interface {
 	Token() string
 }
-
-
 
 type Client struct {
 	client *tgbotapi.BotAPI
@@ -19,7 +17,7 @@ type Client struct {
 
 func New(tokenGetter TokenGetter) (*Client, error) {
 	client, err := tgbotapi.NewBotAPI(tokenGetter.Token())
-	if err != nil{
+	if err != nil {
 		return nil, errors.Wrap(err, "NewBotAPI")
 	}
 	return &Client{
@@ -27,30 +25,30 @@ func New(tokenGetter TokenGetter) (*Client, error) {
 	}, nil
 }
 
-func (c *Client) SendMessage(text string, userID int64) error{
+func (c *Client) SendMessage(text string, userID int64) error {
 	_, err := c.client.Send(tgbotapi.NewMessage(userID, text))
-	if err != nil{
+	if err != nil {
 		return errors.Wrap(err, "client.Send")
 	}
 	return nil
 }
 
-func (c *Client) ListenUpdates(msgModel *messages.Model){
+func (c *Client) ListenUpdates(msgModel *messages.Model) {
 	u := tgbotapi.NewUpdate(0)
 	u.Timeout = 60
 
 	updates := c.client.GetUpdatesChan(u)
 	log.Println("listening for messages")
 
-	for update := range updates{
-		if update.Message != nil{
+	for update := range updates {
+		if update.Message != nil {
 			log.Printf("[%s] %s", update.Message.From.UserName, update.Message.Text)
 
 			err := msgModel.IncomingMessage(messages.Message{
-				Text: update.Message.Text,
+				Text:   update.Message.Text,
 				UserID: update.Message.From.ID,
 			})
-			if err != nil{
+			if err != nil {
 				log.Println("error processing message:", err)
 			}
 		}
